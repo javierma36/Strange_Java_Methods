f1ab1b5430ed64ae1f274fd1aa8c7904fd70d18d
mauriciobedoya <mauriciobedoya@192.168.0.5>
2018-12-17 12:18:34 -0500

    Collections.sort()_JVM_behaviour version 1

---
+++ src/Classes/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author mauriciobedoya
+ */
+public class Student extends Person{
+    public Student(int age){
+        super(age);
+    }
+}
